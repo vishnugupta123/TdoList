deba542a57cd4a5527fa1aaac1c3440f4f9cd9c8
vishnugupta123 <vishnug5694@gmail.com>
2022-07-08 02:06:37 +0530

    Adding procfile and git gitignore

--- app.js
+++ app.js
@@ -125,8 +125,15 @@ app.post("/delete", function (req, res) {
 app.get("/about", function (req, res) {
   res.render("about");
 });
+ 
+let port=process.env.PORT;
+if(port==null || port=="")
+{
+  port =3000;
+}
+app.listen(port);
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("server is listening at Port 3000");
 });
 
